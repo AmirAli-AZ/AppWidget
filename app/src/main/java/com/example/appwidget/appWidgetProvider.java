a99f78406c26ae7e1fad2bb6d6c698838bb842e9
AmirAli-AZ <84965706+AmirAli-AZ@users.noreply.github.com>
2021-11-23 12:50:38 +0330

    Initial commit

---
+++ app/src/main/java/com/example/appwidget/appWidgetProvider.java
@@ -0,0 +1,46 @@
+package com.example.appwidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.widget.RemoteViews;
+
+import static com.example.appwidget.widgetConfig.PREFS;
+import static com.example.appwidget.widgetConfig.KEY_TEXT;
+import static com.example.appwidget.widgetConfig.COLOR_KEY;
+
+public class appWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for(int id : appWidgetIds){
+            // setting intent for main activity
+            Intent intent = new Intent(context , MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context , 0 , intent , 0 );
+            SharedPreferences pref = context.getSharedPreferences(PREFS , context.MODE_PRIVATE);
+            //get values
+            String text = pref.getString(KEY_TEXT + id , "Hi");
+            int color = pref.getInt(COLOR_KEY + id , Color.WHITE);
+
+            RemoteViews views = new RemoteViews(context.getPackageName() , R.layout.widget_layout);
+            views.setOnClickPendingIntent(R.id.text , pendingIntent);
+            // set Text
+            views.setCharSequence(R.id.text , "setText" , text);
+            // set background color
+            views.setInt(R.id.linear1 , "setBackgroundColor" , color);
+            // set text color
+            views.setTextColor(R.id.text , isColorDark(color) ? Color.WHITE : Color.BLACK);
+
+            // update app widget
+            appWidgetManager.updateAppWidget(id , views);
+        }
+    }
+    public boolean isColorDark(int color){
+        double darkness = 1-(0.299*Color.red(color) + 0.587*Color.green(color) + 0.114*Color.blue(color))/255;
+        if(darkness<0.5) return false;
+        else return true;
+    }
+}
---
+++ app/src/main/java/com/example/appwidget/widgetConfig.java
@@ -0,0 +1,137 @@
+package com.example.appwidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.GradientDrawable;
+import android.view.View;
+import android.widget.*;
+import androidx.annotation.ColorInt;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Color;
+import com.coderz.f1.customdialogs.colordialog.ColorDialog;
+
+public class widgetConfig extends AppCompatActivity {
+    private TextView colorValue;
+    private TextView color;
+    private LinearLayout linear1;
+    private EditText editText;
+    public static String PREFS = "PREFS";
+    public static String KEY_TEXT = "TEXT";
+    public static String COLOR_KEY = "COLOR";
+    private int id = AppWidgetManager.INVALID_APPWIDGET_ID;
+    private @ColorInt int appWidgetColor = Color.WHITE;
+    private String text = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.widget_config);
+
+        color = findViewById(R.id.color);
+        colorValue = findViewById(R.id.colorValue);
+        linear1 = findViewById(R.id.linear1);
+        editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+        LinearLayout linear2 = findViewById(R.id.linear2);
+
+        Intent configIntent = getIntent();
+        Bundle extras = configIntent.getExtras();
+        if(extras != null){
+            id = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID , AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+        Intent result = new Intent();
+        result.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID , id);
+        setResult(RESULT_CANCELED , result);
+        if(id == AppWidgetManager.INVALID_APPWIDGET_ID) finish();
+
+        colorValue.setText(intTohex(appWidgetColor));
+
+        GradientDrawable ui = new GradientDrawable();
+        ui.setColor(Color.WHITE);
+        ui.setCornerRadius(getDensity() * 16);
+        linear2.setElevation(getDensity() * 4);
+        linear2.setBackground(ui);
+
+        setColorButton(Color.WHITE);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text = editText.getText().toString();
+                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(widgetConfig.this);
+                Intent intent = new Intent(widgetConfig.this , MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(widgetConfig.this , 0 , intent , 0);
+
+                RemoteViews views = new RemoteViews(getApplicationContext().getPackageName() , R.layout.widget_layout);
+
+                views.setOnClickPendingIntent(R.id.text , pendingIntent);
+                views.setCharSequence(R.id.text , "setText" , text);
+                views.setInt(R.id.linear1 , "setBackgroundColor" , appWidgetColor);
+                views.setTextColor(R.id.text , isColorDark(appWidgetColor) ? Color.WHITE : Color.BLACK);
+
+                appWidgetManager.updateAppWidget(id , views);
+
+                SharedPreferences pref = getSharedPreferences(PREFS , MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString(KEY_TEXT + id , text);
+                editor.putInt(COLOR_KEY + id , appWidgetColor);
+                editor.apply();
+
+                Intent resultValue = new Intent();
+                resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID , id);
+                setResult(RESULT_OK);
+                finish();
+            }
+        });
+
+        color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Color piker by TheRandomCrafter83
+                ColorDialog cd = new ColorDialog(widgetConfig.this, new ColorDialog.DialogResponseListener() {
+                    @Override
+                    public void onOkClicked(int c) {
+                        appWidgetColor = c;
+                        colorValue.setText(intTohex(c));
+                        setColorButton(c);
+                    }
+
+                    @Override
+                    public void onCancelClicked() {
+
+                    }
+                });
+                cd.setTitle("Choose a color");
+                cd.setTabIndex(ColorDialog.TabIndex.PALETTE);
+                cd.setInitialColor(Color.RED);
+                cd.setMargins(8);
+                cd.setBackgroundColor(Color.GRAY);
+                cd.setTextColor(Color.YELLOW);
+                cd.showDialog();
+            }
+        });
+
+    }
+
+    private boolean isColorDark(@ColorInt int color){
+        double darkness = 1-(0.299*Color.red(color) + 0.587*Color.green(color) + 0.114*Color.blue(color))/255;
+        if(darkness<0.5) return false;
+        else return true;
+    }
+    private void setColorButton(@ColorInt int c){
+        GradientDrawable ui2 = new GradientDrawable();
+        ui2.setColor(c);
+        ui2.setCornerRadius(getDensity() * 5);
+        ui2.setStroke((int)(getDensity() * 2), Color.BLACK);
+        color.setBackground(ui2);
+    }
+    private float getDensity(){
+        return getApplicationContext().getResources().getDisplayMetrics().density;
+    }
+    private String intTohex(@ColorInt int color){
+        return String.format("#%06X", (0xFFFFFF & color));
+    }
+}
